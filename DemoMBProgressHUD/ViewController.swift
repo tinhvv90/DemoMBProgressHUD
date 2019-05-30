24be01828c09c1079ba39208f17644e270e719bc
TinhVV <tinhvv90@gmail.com>
2019-05-30 10:34:32 +0700

    Initial Commit

---
+++ DemoMBProgressHUD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoMBProgressHUD
+//
+//  Created by CenHomes on 5/30/19.
+//  Copyright © 2019 Simson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
